f420764a7fde429d81f72b62137a7d56b0ed7851
Matt Huston <matthuston1@aol.com>
2018-08-22 13:47:16 -0600

    working app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var jokeList = document.getElementById('jokeList')
 var regJokes = document.getElementById('jokes')
 var oneLiners = document.getElementById('oneLiners')
 var cleanJokes = document.getElementById('cleanJokes')
-let jokesUrl = 'https://www.reddit.com/r/jokes/hot.json'
+let jokesUrl = 'https://www.reddit.com/r/jokes/hot/.json'
 let oneLinersUrl = 'https://www.reddit.com/r/oneliners/hot.json'
 let cleanJokesUrl = 'https://www.reddit.com/r/cleanjokes/hot.json'
 
@@ -23,15 +23,59 @@ function regularJokes(event) {
         })
 }
 
+// function jokes(myJSON) {
+
+    // let array = []
+    // let dataArray = myJSON.data.children
+    // array.push(dataArray)
+    // console.log(array)
+    // let shuffled = []
+    // while (array[0].length > 0) {
+    //     console.log(array[0].length)
+    // }
+    // let random = dataArray[Math.floor(Math.random() * dataArray.length)]
+
+    // if (random.data.selftext.length < 100) {
+    
+    //     jokeTitle.textContent = random.data.title
+    //     jokeDescrip.textContent = random.data.selftext
+    //     jokeList.appendChild(jokeTitle)
+    //     jokeList.appendChild(jokeDescrip)
+    // }
+// }
+
+// function randomNumbers() {
+//     var numbers = [];
+//     for (var i = 1; i <= 25; i++) {
+//         numbers.push(i);
+//     }
+
+//     var shuffled = [];
+//     while (numbers.length > 0) {
+//         // Remove a random number from the array.
+//         // splice() returns a one-element array.
+//         console.log(numbers.length);
+//         var pick = numbers.splice(Math.floor(Math.random() * numbers.length), 1);
+
+//         // Add it to the shuffled array.
+//         shuffled.push(pick[0]);
+//     }
+
+//     return shuffled;
+// }
 
 function jokes(myJSON) {
-    let random = myJSON.data.children[Math.floor(Math.random() * myJSON.data.children.length)]
-        if (random.data.selftext.length < 100) {
-            jokeTitle.textContent = random.data.title
-            jokeDescrip.textContent = random.data.selftext
-            jokeList.appendChild(jokeTitle)
-            jokeList.appendChild(jokeDescrip)
-        }
+
+    let dataArray = myJSON.data.children
+    let random = dataArray[Math.floor(Math.random() * dataArray.length)]
+
+    if (random.data.selftext.length < 100) {
+    
+        jokeTitle.textContent = random.data.title
+        jokeDescrip.textContent = random.data.selftext
+        jokeList.appendChild(jokeTitle)
+        jokeList.appendChild(jokeDescrip)
+    }
 }
 
 function oneLinerJokes(event) {
@@ -45,18 +89,19 @@ function oneLinerJokes(event) {
 }
 
 function appendOneLiners(myJSON) {
-    
-    let random = myJSON.data.children[Math.floor(Math.random() * myJSON.data.children.length)]
-        if (random.data.selftext.length === 0) {
-            jokeTitle.textContent = random.data.title
-            jokeDescrip.textContent = random.data.selftext
-            jokeList.appendChild(jokeTitle)
-            jokeList.appendChild(jokeDescrip)        
-        }
+
+    let dataArray = myJSON.data.children
+    let random = dataArray[Math.floor(Math.random() * dataArray.length)]
+    if (random.data.selftext.length === 0) {
+        jokeTitle.textContent = random.data.title
+        jokeDescrip.textContent = random.data.selftext
+        jokeList.appendChild(jokeTitle)
+        jokeList.appendChild(jokeDescrip)
+    }
 }
 
 function clean(event) {
-    
+
     fetch(cleanJokesUrl)
         .then(response => response.json())
         .then(myJSON => {
@@ -67,13 +112,15 @@ function clean(event) {
 
 function appendCleanJokes(myJSON) {
 
-    let random = myJSON.data.children[Math.floor(Math.random() * myJSON.data.children.length)]
+    let dataArray = myJSON.data.children
+    let random = dataArray[Math.floor(Math.random() * dataArray.length)]
 
-        if (random.data.selftext.length < 100) {
-            jokeTitle.textContent = random.data.title
-            jokeDescrip.textContent = random.data.selftext
-            jokeList.appendChild(jokeTitle)
-            jokeList.appendChild(jokeDescrip)
-        }
+    if (random.data.selftext.length < 100) {
+        jokeTitle.textContent = random.data.title
+        jokeDescrip.textContent = random.data.selftext
+        jokeList.appendChild(jokeTitle)
+        jokeList.appendChild(jokeDescrip)
+    }
+
 }
 
