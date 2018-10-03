9aaee18b028d5a7359a8c378ac8ba80772872b7b
Matt Huston <matthuston1@aol.com>
2018-10-03 14:21:28 -0600

    removed commented code

--- app.js
+++ app.js
@@ -23,47 +23,6 @@ function regularJokes(event) {
         })
 }
 
-// function jokes(myJSON) {
-
-    // let array = []
-    // let dataArray = myJSON.data.children
-    // array.push(dataArray)
-    // console.log(array)
-    // let shuffled = []
-    // while (array[0].length > 0) {
-    //     console.log(array[0].length)
-    // }
-    // let random = dataArray[Math.floor(Math.random() * dataArray.length)]
-
-    // if (random.data.selftext.length < 100) {
-    
-    //     jokeTitle.textContent = random.data.title
-    //     jokeDescrip.textContent = random.data.selftext
-    //     jokeList.appendChild(jokeTitle)
-    //     jokeList.appendChild(jokeDescrip)
-    // }
-// }
-
-// function randomNumbers() {
-//     var numbers = [];
-//     for (var i = 1; i <= 25; i++) {
-//         numbers.push(i);
-//     }
-
-//     var shuffled = [];
-//     while (numbers.length > 0) {
-//         // Remove a random number from the array.
-//         // splice() returns a one-element array.
-//         console.log(numbers.length);
-//         var pick = numbers.splice(Math.floor(Math.random() * numbers.length), 1);
-
-//         // Add it to the shuffled array.
-//         shuffled.push(pick[0]);
-//     }
-
-//     return shuffled;
-// }
-
 function jokes(myJSON) {
 
     let dataArray = myJSON.data.children
